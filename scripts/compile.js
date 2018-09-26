90824fc5e8cea8c882c43a7fa3558e8abe83f622
brucefeng <clovem.feng@gmail.com>
2018-09-26 16:35:15 +0800

    保存编译后的Car.JSON文件

--- scripts/compile.js
+++ scripts/compile.js
@@ -1,9 +1,22 @@
-const fs = require("fs")
+const fs = require("fs-extra")
 const path = require("path")
 const solc = require("solc")
 
+//cleanup
+const compiledDir = path.resolve(__dirname,'../compiled')
+
+//compile
 const contractPath = path.resolve(__dirname,"../contracts","Car.sol")
 const contractSource = fs.readFileSync(contractPath,"UTF8")
 
 const result = solc.compile(contractSource,1)
 console.log(result)
+
+//save to disk 
+Object.keys(result.contracts).forEach(name =>{
+    const contractName = name.replace(/^:/,'')
+  //  const filePath = path.resolve(__dirname,'../complied',`${contractName}.json`)
+    const filePath = path.resolve(compiledDir,`${contractName}.json`)
+    fs.outputJsonSync(filePath,result.contracts[name])
+    console.log(`save compiled contract ${contractName} to ${filePath}`)
+})
